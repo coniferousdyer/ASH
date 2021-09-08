0c304395bbe488e1c59012c8934fb7bcfd33313b
Arjun Muraleedharan <arjunmcd0321@gmail.com>
2021-09-09 04:01:17 +0530

    Added modifyPath()

--- src/main.c
+++ src/main.c
@@ -4,11 +4,12 @@
 #include <string.h>
 #include <unistd.h>
 
-// String storing hostname
+// Important global strings
 char HOSTNAME[HOST_NAME_MAX + 1];
 char USERNAME[LOGIN_NAME_MAX + 1];
 char INPUTSTRING[MAX_COMMAND_LENGTH + 1];
 char COMMANDSTRING[MAX_COMMAND_LENGTH + 1];
+char PATH[MAX_PATH_LENGTH + 1];
 
 int main(void)
 {
@@ -18,9 +19,10 @@ int main(void)
     // The array containing the command-line arguments entered
     char *args[MAX_ARG_NO + 1];
 
-    // Obtaning username and hostname
+    // Obtaning username, hostname and path of current working directory (the home directory for this shell)
     getlogin_r(USERNAME, LOGIN_NAME_MAX + 1);
     gethostname(HOSTNAME, HOST_NAME_MAX + 1);
+    char *HOME = getcwd(PATH, MAX_PATH_LENGTH + 1);
 
     // Printing intro
     printf("\nWELCOME TO A-SHELL!\n\n");
--- src/utilities.c
+++ src/utilities.c
@@ -43,6 +43,42 @@ void parseInput(char *inputString, char *parsedString)
     parsedString[len] = '\0';
 }
 
+// Function to modify the path
+void modifyPath(char *path, char *subpath)
+{
+    // Splitting the subpath into the respective directories
+    char *directory = strtok(subpath, "/");
+    while (directory != NULL)
+    {
+        // Checking if we're going back or front
+        if (strcmp(directory, "..") == 0)
+        {
+            // Checking if the absolute path is currently the root directory
+            if (strcmp("path", "/") == 0)
+                continue;
+
+            // i stores the index of the last '/' in the path
+            int i;
+            for (i = strlen(path) - 1; path[i] != '/'; i--)
+                ;
+
+            // Checking if we are in the home directory and going to root directory
+            if (i == 0)
+                path[i + 1] = '\0';
+            else
+                path[i] = '\0';
+        }
+        else
+        {
+            if (path[strlen(path) - 1] != '/')
+                strcat(path, "/");
+            strcat(path, directory);
+        }
+
+        directory = strtok(NULL, "/");
+    }
+}
+
 // Function to execute the corresponding command
 void execCommand(char *args[], int argc)
 {
